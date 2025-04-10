bccce7e9dbd1fec2347420cb4fb12666300e1ca3
JDGarciag12 <jdgarciag12uniandes.edu.co>
2025-04-10 14:29:07 -0500

    creacion rbt

---
+++ DataStructures/Tree/red_black_tree.py
@@ -0,0 +1,11 @@
+from DataStructures.Tree import rbt_node as rbt
+def new_tree():
+    root = None
+    return None
+
+def is_red(node_rbt):
+    bol = rbt.is_red(node_rbt)
+    if bol:
+        return True
+    else:
+        return False
